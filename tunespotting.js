568bea6fd43d362612897cfcf8f6562ec26f04b8
Thomas Crescenzi <trescenzi@gmail.com>
2012-09-15 22:20:37 +0900

    parsing of get responses complete

--- tunespotting.js
+++ tunespotting.js
@@ -1,5 +1,16 @@
 var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var player = models.player;
+var Artist = models.Artist;
+var lastFMAPIRoot = "http://ws.audioscrobbler.com";
 var auth = sp.require('sp://import/scripts/api/auth');
+
+var lastFMKey='ce896902c7da0790956cec280a9c1df4';
+
+exports.init = init;
+
+function init() {
+}
 
 function authFM(){
 auth.showAuthenticationDialog('http://www.last.fm/api/auth/?api_key=ce896902c7da0790956cec280a9c1df4',
@@ -7,7 +18,8 @@ auth.showAuthenticationDialog('http://www.last.fm/api/auth/?api_key=ce896902c7da
 
     onSuccess : function(response) {
         // Response will be something like 'sp://my_app_name?token=xxxxxxx'
-        console.log("Success! Here's the response URL: " + response);
+        var responses = parseGetResponse(response);
+        localStorage['lastFMToken'] = responses['token'];
     },
 
     onFailure : function(error) {
@@ -18,77 +30,30 @@ auth.showAuthenticationDialog('http://www.last.fm/api/auth/?api_key=ce896902c7da
 });
 }
 
+function getLastFMSession(){
+  var apiCall = "/2.0/?method=auth.gettoken&";
+}
 
+function test(){
+  console.log(localStorage['lastFMToken']);
+}
 
-// var sp = getSpotifyApi(1);
-// var models = sp.require('sp://import/scripts/api/models');
-// var player = models.player;
-// var Artist = models.Artist;
-// var lastFMAPIRoot = "http://ws.audioscrobbler.com";
-// var auth = sp.require('sp://import/scripts/api/auth');
+function getRequest(url){
+  var request = new XMLHttpRequest();
 
-// var lastFMKey='ce896902c7da0790956cec280a9c1df4';
+  request.open("GET", url, false);
+  console.log(url);
+  request.send();
+  return request.responseText;
+}
 
-// exports.init = init;
+function parseGetResponse(response){
+  var splitUrl = response.split("?")[1].split("&");
+  var variables = new Array();
+  for(var i=0; i<splitUrl.length; i++){
+    var broken = splitUrl[i].split("=");
+    variables[broken[0]] = broken[1];
+  }
+  return variables;
+}
 
-
-// function init() {
-// }
-
-// function authFM(){
-//   auth.showAuthenticationDialog('http://www.last.fm/api/auth/?api_key=ce896902c7da0790956cec280a9c1df4&cb=sp://tunespotting'
-//     , 'sp://tunespotting', {
-
-//   onSuccess : function(response) {
-//     // Response will be something like 'sp://my_app_name?token=xxxxxxx'
-//     console.log("Success! Here's the response URL: " + response);
-//   },
-
-//   onFailure : function(error) {
-//     console.log("Authentication failed with error: " + error);
-//   },
-
-//   onComplete : function() { 
-//     console.log("something happened");
-//   }
-//   });
-// }
-
-
-// function getLastFMAuthToken(){
-//   var apiCall = "/2.0/?method=auth.gettoken&api_key="+lastFMKey+"&format=json";
-//   var url = lastFMAPIRoot+apiCall;
-//   return getRequest(url);
-// }
-
-// function lastFMAuth(){
-//   var token = JSON.parse(getLastFMAuthToken());
-//   console.log(token);
-//   if(localStorage['userPermission?'] != true)
-//     getLastFMUserPermission(token);
-// }
-
-// function getLastFMUserPermission(token){
-//   //url should look like:
-//   //http://www.last.fm/api/auth/?api_key=xxxxxxxxxxx&token=xxxxxxxx
-//   var url = "https://www.last.fm/api/auth/?api_key="+lastFMKey+"&token="+token['token'];
-//   getRequest(url);
-// }
-
-// function getLastFMUserName(){
-//   return localStorage['lastFMUserName'];
-// }
-
-// function setLastFMUserName(){
-//   localStorage['lastFMUser'] = $('[name=username]').val();
-// }
-
-
-// function getRequest(url){
-//   var request = new XMLHttpRequest();
-
-//   request.open("GET", url, false);
-//   console.log(url);
-//   request.send();
-//   return request.responseText;
-// }
